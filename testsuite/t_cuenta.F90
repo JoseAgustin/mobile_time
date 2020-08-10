9f9c1798b69ba36c338d9c894c20fa46579a1469
Agustin Garcia <agustin@atmosfera.unam.mx>
2020-08-10 11:29:34 -0500

    Update test1

--- testsuite/t_cuenta.F90
+++ testsuite/t_cuenta.F90
@@ -1,9 +1,7 @@
 program test1
+use grid_temp_mod
 implicit none
-integer :: iunit
-integer :: cuenta
-open(newunit=iunit,file="f_test.csv",status="old",action="read")
-
- print *,cuenta(iunit)
-
+	integer :: iunit
+	open(newunit=iunit,file="f_test.csv",status="old",action="read")
+	print *,cuenta(iunit)
 end program test1
